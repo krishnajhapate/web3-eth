b8a58b21a315272e16cf60f0854047b550b5367a
krishnajhapate <ki54.jhapate@gmail.com>
2021-12-13 08:41:59 +0530

    base setup

--- app.py
+++ app.py
@@ -2,5 +2,12 @@ from web3 import Web3
 
 infura_url = "https://mainnet.infura.io/v3/16e2559e46ba4b85b66742b5377104ed"
 
-web3 = Web3(Web3.HTTPProvider(infura_url)
-)
+web3 = Web3(Web3.HTTPProvider(infura_url))
+
+# status of connection
+print(web3.isConnected())
+
+# Get block number info
+print(web3.eth.blockNumber)
+
+# balance = web3.getBalace("")
